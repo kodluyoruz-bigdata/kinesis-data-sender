6c799b59ca14df4395dfb01a1a49e7a178f4bfd7
Yaşarcan Yılmaz <yasarcanyilmaz@yahoo.com.tr>
2018-09-15 10:13:30 +0300

    Change sender to Firehose sender

--- src/main/scala/me/ycan/KinesisSender.scala
+++ src/main/scala/me/ycan/KinesisSender.scala
@@ -1,44 +1,55 @@
 package me.ycan
 
 import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
 
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
-import com.amazonaws.services.kinesis.model.PutRecordRequest
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.kinesisfirehose.{AmazonKinesisFirehoseAsyncClient, AmazonKinesisFirehoseClient}
+import com.amazonaws.services.kinesisfirehose.model.{PutRecordBatchRequest, Record}
 
 import scala.io.Source
+import scala.collection.JavaConverters._
 
 object KinesisSender {
 
   def main(args: Array[String]): Unit = {
 
-    val regionName = args(0)
+    val regionName = Regions.EU_WEST_1.getName
     val streamName = args(1)
     val key        = args(2)
     val secret     = args(3)
     val filePath   = args(4)
 
-    val kinesisClient = AmazonKinesisClientBuilder
-      .standard()
+    val firehoseClient = AmazonKinesisFirehoseAsyncClient
+      .asyncBuilder()
       .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(key, secret)))
       .withRegion(regionName)
       .build()
 
-    val records = Source.fromFile(filePath).getLines()
+    val lines = Source.fromFile(filePath).getLines()
+    val groupedLines = lines.grouped(500)
 
-    records.foreach { r =>
-      val record = createPutRecord(streamName, 1, r)
-      val result = kinesisClient.putRecord(record)
-      println("Put Result: " + result)
+    val records = groupedLines.map{ lines =>
+      val records: Seq[Record] = lines.map(line => createRecord(line))
+      val batchPutRecords = new PutRecordBatchRequest().withDeliveryStreamName(streamName).withRecords(records.asJavaCollection)
+      firehoseClient.putRecordBatch(batchPutRecords)
+
     }
+
+    records.foreach{results =>
+      println(results.getFailedPutCount)
+
+    }
+
+
 
   }
 
-  def createPutRecord(streamName: String, partitionKey: Int, inputData: String): PutRecordRequest = {
+  def createRecord(inputData: String): Record = {
 
-    new PutRecordRequest()
-      .withStreamName(streamName)
-      .withPartitionKey(partitionKey.toString)
+    new Record()
+
       .withData(ByteBuffer.wrap((inputData + "\n").getBytes))
   }
 
